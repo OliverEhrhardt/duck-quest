125290176ed85950f75086093c341acfb25cf6a2
Oliver Ehrhardt <oliver.ehrhardt0@gmail.com>
2016-12-14 21:06:04 -0700

    added collision test to character class

--- src/Character.js
+++ src/Character.js
@@ -1,23 +1,33 @@
 import {Sprite} from 'EaselJS';
+import {checkPixelCollision} from 'CollisionJS';
 
 class Character extends Sprite {
 	constructor(width, height, x, y, spriteSheet){
 	  	super(spriteSheet);
+	  	this.gotoAndPlay("stand");
 	  	this.x = x;
 	  	this.y = y;
 	  	this.height = height;
 	  	this.width = width
 	  	this.speed = 5;
+	  	let pt = this.globalToLocal(width/2, height/2)
+	  	this.regX = pt.x; this.regY = pt.y;
 
 	  	this.scaleX = width/spriteSheet._frameWidth;
 	  	this.scaleY = height/spriteSheet._frameHeight;
 
 	  	let keyMap = {};
-		const handleDown = (event) => { keyMap[event.key] = true };
+		const handleDown = (event) => { 
+			if(event.key.match(/(w|a|s|d|ArrowUp|ArrowLeft|ArrowRight|ArrowDown)/)){
+				keyMap[event.key] = true; 
+			} 
+		};
 		document.onkeydown = handleDown;
-		const handleUp = (event) => { 
-			keyMap[event.key] = false; 
-			this.gotoAndStop('stand'); 
+		const handleUp = (event) => {
+			if(event.key.match(/(w|a|s|d|ArrowUp|ArrowLeft|ArrowRight|ArrowDown)/)){
+				keyMap[event.key] = false; 
+				this.gotoAndPlay('stand'); 				
+			} 
 		};
 		document.onkeyup = handleUp;
 
@@ -25,9 +35,15 @@ class Character extends Sprite {
 			if(keyMap['w']||keyMap['a']||keyMap['s']||keyMap['d']||keyMap['ArrowUp']||keyMap['ArrowLeft']||keyMap['ArrowDown']||keyMap['ArrowRight']){
 				if(this.currentAnimation === "stand") this.gotoAndPlay('run');
 				if(keyMap['w'] || keyMap['ArrowUp']) this.y -= this.speed;
-				if(keyMap['a'] || keyMap['ArrowLeft']) this.x -= this.speed;
+				if(keyMap['a'] || keyMap['ArrowLeft']){
+					this.x -= this.speed;
+					if(this.scaleX > 0) this.scaleX = -this.scaleX;
+				} 
 				if(keyMap['s'] || keyMap['ArrowDown']) this.y += this.speed;
-				if(keyMap['d'] || keyMap['ArrowRight']) this.x += this.speed;
+				if(keyMap['d'] || keyMap['ArrowRight']){
+					this.x += this.speed;
+					if(this.scaleX < 0) this.scaleX = -this.scaleX;
+				} 
 			} 
 		}
 		this.on("tick", handleMovement);
@@ -41,6 +57,10 @@ class Character extends Sprite {
 			window.addEventListener('keydown', handleDown);
 			window.addEventListener('keyup', handleUp);
 		}
+	}
+
+	checkCollision(target){
+		return checkPixelCollision(this, target, 0.8)
 	}
 }
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Character from './Character.js';
 import {checkPixelCollision} from 'CollisionJS';
 
 const manifest = [
-	{src:"src/img/Characters/Character/duck.json", id:"duck", type:"spritesheet"}
+	{src:"src/img/Characters/Enemies/bat.json", id:"bat", type:"spritesheet"}
 ]
 
 document.getElementById("canvas").focus();
@@ -12,14 +12,15 @@ document.getElementById("canvas").focus();
 const stage = new Stage("canvas");
 const loader = new LoadQueue(false);
 loader.on('fileload', (event)=>{
-	let duck = new Character(50,50,0,0,event.result);
+	let duck = new Character(50,50,50,50,event.result);
 	let duck2 = new Character(50, 50, 100, 100, event.result);
 
+	// duck.speed = 20
+
 	duck.giveControl();
-	duck2.cloneControl();
 	stage.addChild(duck, duck2);
 	Ticker.on("tick", () => {
-		// console.log(checkPixelCollision(duck, duck2));
+		console.log(duck.checkCollision(duck2));
 		stage.update();
 	});
 
